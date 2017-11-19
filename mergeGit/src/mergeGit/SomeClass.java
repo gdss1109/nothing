357004254adb4fbc7e94c8783efd318750565444
Gagandeep Singh <gdss1109@gmail.com>
2017-11-19 13:51:29 -0800

    add new output

--- mergeGit/src/mergeGit/SomeClass.java
+++ mergeGit/src/mergeGit/SomeClass.java
@@ -3,6 +3,7 @@ package mergeGit;
 public class SomeClass {
 	public void again() {
 		System.out.println("I couldn't repair your breaks, so I made your horn louder");
+		System.out.println("Testing 123");
 	}
 	
 	public static void main(String[] args) {
