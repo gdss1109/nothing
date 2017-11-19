8bfa23096c49428765d7940fc0d67f1a5f24f905
Gagandeep Singh <gagandeepsingh@Gagandeeps-MacBook-Air.local>
2017-11-19 13:44:20 -0800

    4th

--- mergeGit/src/mergeGit/SomeClass.java
+++ mergeGit/src/mergeGit/SomeClass.java
@@ -1,14 +1,14 @@
 package mergeGit;
 
 public class SomeClass {
-	public void newFuncName() {
+	public void again() {
 		System.out.println("I couldn't repair your breaks, so I made your horn louder");
 	}
 	
 	public static void main(String[] args) {
 		
 		SomeClass someclass  = new SomeClass();
-		someclass.newFuncName();
+		someclass.again();
 		
 	}
 }
